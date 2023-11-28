a1dcb3cb90285cbdffcec9c100949fe9a943d41a
Gustaf Hammarberg <gustaf@waveworks.dk>
2023-11-28 21:55:37 +0100

    Added memory calculation to main script for simplicity.

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 import os
 import keras
+import utils.calc_mem
 from keras.models import Sequential
 from keras.layers import Dense, Conv1D, Flatten
 from matplotlib import pyplot as plt
@@ -136,9 +137,9 @@ with open("../ESP-32/main/model.c", 'w') as c_file:
             c_file.write('\n')
     c_file.write('\n};\n')
 
-
-    # Save TensorFlow Lite model
-with open("loop_model.tflite", "wb") as f:
+# Save TensorFlow Lite model and print memory
+with open("model.tflite", "wb") as f:
     f.write(tflite_model)
+utils.calc_mem.calc_mem("model.tflite")
 
 print('Done.')
