3f7b94f825b7526043e42173f430306360e001e0
Steffen Breinbjerg <steffenpb@outlook.dk>
2023-12-18 11:53:55 +0100

    Added quantization for main and esp32_metric plus small test python script.

--- Python/main.py
+++ Python/main.py
@@ -9,10 +9,27 @@ from utils.calc_mem import calc_mem
 from utils.export_tflite import write_model_h_file, write_model_c_file
 from utils.plots import plot_predictions_vs_labels, plot_learning_curves
 
+# Enable Quantization
+ENABLE_QUANTIZATION = False
+
+# Set file names depending on quantization
+if ENABLE_QUANTIZATION:
+    FILE_NAME = "model_quantized"
+else:
+    FILE_NAME = "model"
+
+
+def representative_dataset():
+    global x_test
+    x_test_samples = x_test[np.random.choice(x_test.shape[0], 100, replace=False)]
+    yield [x_test_samples.astype(np.float32)]
+
+
 # Minimize TensorFlow logging
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
-tf.get_logger().setLevel('ERROR')
+
+tf.get_logger().setLevel("ERROR")
 
 # Preprocess data if not done already
 if not os.path.exists('gen/x.npy') or not os.path.exists('gen/y.npy'):
@@ -26,8 +43,12 @@ y = np.load('gen/y.npy')
 # plot_dataset(x, y, block=True)
 
 # Split into training, validation and test sets
-x_train, x_val, x_test = np.split(x, [int(.6 * len(x)), int(.8 * len(x))])
-y_train, y_val, y_test = np.split(y, [int(.6 * len(y)), int(.8 * len(y))])
+x_train, x_val, x_test = np.split(x, [int(0.6 * len(x)), int(0.8 * len(x))])
+y_train, y_val, y_test = np.split(y, [int(0.6 * len(y)), int(0.8 * len(y))])
+
+# Save x/y test for tflite test
+np.save("gen/x_test.npy", x_test)
+np.save("gen/y_test.npy", y_test)
 
 # Shuffle the training data
 indices = np.arange(len(x_train))
@@ -39,10 +60,10 @@ y_train = y_train[indices]
 num_positives = np.sum(y)
 num_negatives = len(y) - num_positives
 ratio = num_negatives / num_positives
-print('Negative to positive ratio: ', ratio)
+print("Negative to positive ratio: ", ratio)
 
 # Build and compile model
-print('Building model...')
+print("Building model...")
 model = Sequential()
 model.add(Conv1D(8, 3, activation='relu', input_shape=(WINDOW_SIZE, SPECTRUM_SIZE)))  # Output shape (22, 8)
 model.add(MaxPooling1D(2))  # Output shape (11, 8)
@@ -83,31 +104,38 @@ y_pred = model.predict(x_test)
 
 # Print evaluation metrics
 print()
-print('Validation loss:     %.4f' % val_loss)
-print('Validation accuracy: %.4f' % val_accuracy)
-print('Test loss:           %.4f' % test_loss)
-print('Test accuracy:       %.4f' % test_accuracy)
+print("Validation loss:     %.4f" % val_loss)
+print("Validation accuracy: %.4f" % val_accuracy)
+print("Test loss:           %.4f" % test_loss)
+print("Test accuracy:       %.4f" % test_accuracy)
 
 # Print confusion matrix
 y_pred_bool = np.round(y_pred)
 confusion_matrix = np.zeros((2, 2))
 for i in range(len(y_pred_bool)):
     confusion_matrix[int(y_test[i]), int(y_pred_bool[i, 0])] += 1
-print('True positives:     ', int(confusion_matrix[1, 1]))
-print('True negatives:     ', int(confusion_matrix[0, 0]))
-print('False positives:    ', int(confusion_matrix[0, 1]))
-print('False negatives:    ', int(confusion_matrix[1, 0]))
+print("True positives:     ", int(confusion_matrix[1, 1]))
+print("True negatives:     >", int(confusion_matrix[0, 0]))
+print("False positives:    ", int(confusion_matrix[0, 1]))
+print("False negatives:    ", int(confusion_matrix[1, 0]))
 
 # Plot predictions vs labels
 plot_predictions_vs_labels(y_pred, y_test, block=True)
-
 # Convert to TensorFlow Lite model
-print('Converting to TensorFlow Lite model...')
+print("Converting to TensorFlow Lite model...")
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+if ENABLE_QUANTIZATION:
+    print("Quantizing TensorFlow Lite model...")
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.representative_dataset = representative_dataset
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.inference_input_type = tf.int8
+    converter.inference_output_type = tf.int8  # or tf.int8
+
 tflite_model = converter.convert()
 
 # Export TensorFlow Lite model to C source files
-print('Exporting TensorFlow Lite model to C source files...')
+print("Exporting TensorFlow Lite model to C source files...")
 defines = {
     "SAMPLE_RATE": SAMPLE_RATE,
     "FRAME_SIZE": FRAME_SIZE,
@@ -116,20 +144,32 @@ defines = {
     "SPECTRUM_TOP": SPECTRUM_TOP,
     "SPECTRUM_SIZE": SPECTRUM_SIZE,
     "SPECTRUM_MEAN": SPECTRUM_MEAN,
-    "SPECTRUM_STD": SPECTRUM_STD
+    "SPECTRUM_STD": SPECTRUM_STD,
 }
 declarations = [
-    "const unsigned long SPECTRUM_SRC[] = { " + ", ".join(map(str, SPECTRUM_SRC)) + " };",
-    "const unsigned long SPECTRUM_DST[] = { " + ", ".join(map(str, SPECTRUM_DST)) + " };"
+    "const unsigned long SPECTRUM_SRC[] = { "
+    + ", ".join(map(str, SPECTRUM_SRC))
+    + " };",
+    "const unsigned long SPECTRUM_DST[] = { "
+    + ", ".join(map(str, SPECTRUM_DST))
+    + " };",
 ]
-write_model_h_file("../ESP-32/main/model.h", defines, declarations)
-write_model_c_file('../ESP-32/main/model.c', tflite_model)
-write_model_h_file("../esp32_metric/components/models/include_v2/model_v2.h", defines, declarations)
-write_model_c_file('../esp32_metric/components/models/model_v2.c', tflite_model)
+
+# Do not inlcude quantized model into the main project
+if not ENABLE_QUANTIZATION:
+    write_model_h_file("../ESP-32/main/model.h", defines, declarations)
+    write_model_c_file("../ESP-32/main/model.c", tflite_model)
+
+write_model_h_file(
+    f"../esp32_metric/components/models/include_v2/{'model_v2_quan' if ENABLE_QUANTIZATION else 'model_v2'}/{FILE_NAME}_v2.h",
+    defines,
+    declarations,
+)
+write_model_c_file(f"../esp32_metric/components/models/{FILE_NAME}_v2.c", tflite_model)
 
 # Save TensorFlow Lite model and print memory
-with open("gen/model.tflite", "wb") as f:
+with open(f"gen/{FILE_NAME}.tflite", "wb") as f:
     f.write(tflite_model)
-calc_mem("gen/model.tflite")
+calc_mem(f"gen/{FILE_NAME}.tflite")
 
-print('Done.')
+print("Done.")
